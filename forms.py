6d254fa12d8f46532bef463ee7ed7db80df633ed
ArtemLevin <python_study_2022@mail.ru>
2024-01-14 03:12:19 +0300

    prepaing for homework

---
+++ forms.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, PasswordField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+from main import app
+
+app.config['SECRET_KEY'] = b''
+csrf  = CSRFProtect(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('male', 'Мужчина'), ('female', 'Женщина')])
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
