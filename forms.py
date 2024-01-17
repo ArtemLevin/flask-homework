fcb0d3fcc2770262524ce7f2a27e4ed28ac9def9
ArtemLevin <python_study_2022@mail.ru>
2024-01-17 23:27:31 +0300

    hw 3 done

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, PasswordField, IntegerField, SelectField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 from main import app
@@ -8,18 +8,9 @@ from main import app
 app.config['SECRET_KEY'] = b''
 csrf  = CSRFProtect(app)
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-
-
-class RegisterForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
-    gender = SelectField('Gender', choices=[('male', 'Мужчина'), ('female', 'Женщина')])
-
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-from forms import LoginForm, RegistrationForm
-from models import User, Post
-
+from forms import RegistrationForm
+from models import User
+from hashlib import sha256
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://mydatabase.db'
 db = SQLAlchemy(app)
@@ -18,57 +18,19 @@ def init_db():
     db.create_all()
     print('OK')
 
-@app.cli.command("add-john")
-def add_user():
-    user = User(username='john', email='lolkek@gmail.com')
-    db.session.add(user)
-    db.session.commit()
-    print('John successfully added to the database')
 
-@app.cli.command("edit-john")
-def edit_user():
-    user=User.query.filter_by(username='john').first()
-    user.email = 'newlolkek@gmail.com'
-    print('Edit John mail in database')
-
-@app.cli.command('del-john')
-def del_user():
-    user = User.query.filter_by(username='John').first()
-    db.session.delete(user)
-    db.session.commit()
-    print('Delete John from database')
-
-@app.route('users/<username>/')
-def users_by_username(username):
-    users=User.query.filter(User.username == username).all()
-    context = {'users':users}
-    return render_template('users.html', **context)
-
-@app.route('/posts/author/<int:user_id>/')
-def get_posts_by_author(user_id):
-    posts = Post.query.filter_by(author_id=user_id).all()
-    if posts:
-        return jsonify(
-            [{'id': post.id, 'title': post.title, 'content': post.content, 'create_at': post.created_at} for post in posts]
-        )
-    else:
-        return jsonify({'error':' Post not found'}), 404
-
-@app.route('/login/', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if request.method == 'POST' and form.validate():
-        pass
-    return render_template('login.html', form=form)
-
-@app.route('/register', methods=['GET','POST'])
-def register():
+@app.route('/', methods=['GET','POST'])
+def index():
     form = RegistrationForm()
-    if request.method == 'POST' and form.validate():
-        email = form.email.data
-        password = form.password.data
-        print(email, password)
-    return render_template('register.html', form=form)
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    surname=form.username.data,
+                    email=form.email.data,
+                    password=sha256(form.password.data.encode(encoding='utf-8')).hexdigest())
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
