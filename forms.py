fb464522fbe8eeacf72acfbee0de22fe35016c0c
ArtemLevin <python_study_2022@mail.ru>
2024-01-17 22:47:56 +0300

    hw 3 done

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, PasswordField, IntegerField, SelectField
+from wtforms import StringField, PasswordField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 from main import app
@@ -19,7 +19,8 @@ class RegisterForm(FlaskForm):
     gender = SelectField('Gender', choices=[('male', 'Мужчина'), ('female', 'Женщина')])
 
 class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-
+    submit = SubmitField('Submit')
