38c5480b8c9c9a2ebf77b2238fd19ed41bf28623
ArtemLevin <python_study_2022@mail.ru>
2024-01-24 10:22:42 +0300

    hw_5 done

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, Request
+import logging
+from fastapi.responses import HTMLResponse
+import pandas as pd
+from fastapi.templating import Jinja2Templates
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+logging.basicConfig(level=logging.INFO, filename="to_do_tasks.txt", encoding="utf-8")
+logger = logging.getLogger(__name__)
+
+TO_DO_COMMON_LIST = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class To_do_task(BaseModel):
+    id: int
+    header: Optional[str] = None
+    description: Optional[str] = None
+    done_status: bool = False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.post("/tasks/", response_model=To_do_task)
+async def create_item(to_do_task: To_do_task):
+    logger.info(f'Создали задание: {to_do_task}')
+    to_do_task_id = len(TO_DO_COMMON_LIST) + 1
+    to_do_task.id = to_do_task_id
+    TO_DO_COMMON_LIST.append(to_do_task)
+    return to_do_task_id
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get('/tasks/', response_class=HTMLResponse)
+async def users_list(request: Request):
+    logger.info('Создали таблицу заданий')
+    to_do_task_table = pd.DataFrame([vars(to_do_task) for to_do_task in TO_DO_COMMON_LIST]).to_html()
+    return templates.TemplateResponse("tasks.html", {"request": request, "users_table": to_do_task_table})
+
+
+@app.get('/tasks/{id}', response_class=HTMLResponse)
+async def users_list(request: Request, to_do_task: To_do_task):
+    logger.info(f'Выводим задание {to_do_task}')
+    to_do_task_table = pd.DataFrame([vars(to_do_task)]).to_html()
+    return templates.TemplateResponse("tasks.html", {"request": request, "to_do_task_table": to_do_task_table})
+
+
+@app.put('/tasks/{id}', response_model=To_do_task)
+async def update_user_info(to_do_task_id: int, to_do_task: To_do_task):
+    logger.info(f'Обновили задание {to_do_task_id}')
+    for i, current_to_do_task in enumerate(TO_DO_COMMON_LIST):
+        if current_to_do_task.id == to_do_task_id:
+            TO_DO_COMMON_LIST[i] = to_do_task
+            return {"to_do_task_id": to_do_task_id, "to_do_task": to_do_task}
+
+
+@app.delete('/tasks/{id}')
+async def delete_user(to_do_task_id: int):
+    logger.info(f'Удалили задание {to_do_task_id}')
+    for i, current_to_do_task in enumerate(TO_DO_COMMON_LIST):
+        if current_to_do_task.id == to_do_task_id:
+            TO_DO_COMMON_LIST.pop(i)
+        return {"to_do_task_id": to_do_task_id}
+    else:
+        return print("There is no task with such id")
