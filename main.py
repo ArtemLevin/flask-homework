312ca8b9d22c9094dc17190ea12eddac6088b4e7
ArtemLevin <python_study_2022@mail.ru>
2024-01-28 00:40:56 +0300

    hw_6 init commit

--- main.py
+++ main.py
@@ -1,16 +1,103 @@
-# This is a sample Python script.
+from datetime import datetime
+from typing import Optional, List
+from pydantic import BaseModel, Field
+from fastapi import FastAPI, Request
+import logging
+from hashlib import sha256
+from fastapi.responses import HTMLResponse
+import pandas as pd
+from fastapi.templating import Jinja2Templates
+import databases
+import sqlalchemy
+from sqlalchemy import ForeignKey
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+DATABASE_URL = "sqlite:///shop.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+goods = sqlalchemy.Table(
+    "goods",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(32)),
+    sqlalchemy.Column("description", sqlalchemy.String(128)),
+    sqlalchemy.Column("price", sqlalchemy.Integer)
+    #ForeignKey("name", _constraint="orders.id")
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+orders = sqlalchemy.Table(
+    "orders",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer),
+    sqlalchemy.Column("good_id", sqlalchemy.Integer),
+    sqlalchemy.Column("date", sqlalchemy.Date),
+    sqlalchemy.Column("status", sqlalchemy.Boolean)
+)
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("first_name", sqlalchemy.String(32)),
+    sqlalchemy.Column("surname", sqlalchemy.String(32)),
+    sqlalchemy.Column("email", sqlalchemy.String(32)),
+    sqlalchemy.Column("password", sqlalchemy.String(32))
+)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+class User(BaseModel):
+    id: int
+    first_name: str = Field(max_length=32)
+    surname: str = Field(max_length=32)
+    email: str = Field(max_length=32)
+    password: str = Field(max_length=16)
+
+
+class UserIn(BaseModel):
+    first_name: str = Field(max_length=32)
+    surname: str = Field(max_length=32)
+    email: str = Field(max_length=32)
+    password: str = Field(max_length=16)
+
+class Order(BaseModel):
+    id: int
+    user_id: int
+    good_id: int
+    date: datetime
+    status: bool
+
+
+class OrderIn(BaseModel):
+    user_id: int
+    good_id: int
+    date: datetime
+    status: bool
+
+class Good(BaseModel):
+    id: int
+    name: str = Field(max_length=32)
+    description: str = Field(max_length=128)
+    price: float
+
+
+class GoodIn(BaseModel):
+    name: str = Field(max_length=32)
+    description: str = Field(max_length=128)
+    price: float
+@app.on_event("startup")
+async def starup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
